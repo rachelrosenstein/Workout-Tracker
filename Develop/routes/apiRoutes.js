801681bcc405f743dfcd822e163b4ce019efc54e
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-12-27 09:40:12 -0500

    api routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+
+});
+
+router.get("/stats", (req, res) => {
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000
+const morgan = require("morgan");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(morgan("tiny"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
-// routes
-app.use(require("./routes/api.js"));
+app.use(require("./Develop/routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
